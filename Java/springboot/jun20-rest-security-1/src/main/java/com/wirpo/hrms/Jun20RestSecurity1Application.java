5e1a8209acb5e006e8fcf5d397043ad3d8016b44
Jagadeeswaran <rjagadeeswaran@yahoo.com>
2022-06-21 20:31:39 +0530

    jun21

---
+++ Java/springboot/jun20-rest-security-1/src/main/java/com/wirpo/hrms/Jun20RestSecurity1Application.java
@@ -0,0 +1,13 @@
+package com.wirpo.hrms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Jun20RestSecurity1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Jun20RestSecurity1Application.class, args);
+	}
+
+}
---
+++ Java/springboot/jun20-rest-security-1/src/test/java/com/wirpo/hrms/Jun20RestSecurity1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wirpo.hrms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Jun20RestSecurity1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
